b6950f32aa07d50c109b84808b007ac8dc5e1df2
Jernej Sila <jernej.sila@gmail.com>
2016-04-27 10:29:21 +0200

    init

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/JSila/packtFree"
+	"github.com/JSila/packtFree/log"
+	"github.com/JSila/packtFree/notify"
+	"github.com/JSila/packtFree/notify/email"
+	"github.com/JSila/packtFree/notify/email/options"
+	"github.com/JSila/packtFree/notify/stdout"
+)
+
+func main() {
+	var notifier = flag.String("notifier", "stdout", "How to notify (via email or stdout)?")
+	flag.Parse()
+
+	if err := packtFree.Get(); err != nil {
+		log.Log.Panic(err)
+	}
+
+	var n notify.Notifier
+	switch *notifier {
+	case notify.Email:
+		options, err := options.FromFile("config/mailgun.json")
+		if err != nil {
+			log.Log.Panic(err)
+		}
+		n, err = email.New(packtFree.Data, options)
+		if err != nil {
+			log.Log.Panic(err)
+		}
+	default:
+		n = stdout.New(packtFree.Data)
+	}
+
+	users, err := notify.UsersFromFile("config/recipients.json")
+	if err != nil {
+		log.Log.Fatal(err)
+	}
+	for _, user := range users {
+		if err := n.Notify(user); err != nil {
+			log.Log.Error(err)
+			log.Log.Fatal("Can't notify via ", n.Name())
+		}
+	}
+}
